6ddaaf3fe680fd52ef30ded3b274655e6e9dd5b6
unknown <azizur.rahman.ru@gmail.com>
2022-06-16 22:45:42 +0600

    git init

---
+++ Console/ConsoleApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        public static List<int> GetRank(List<int> ranked, List<int> player)
+        {
+            var result = new List<int>();
+
+
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            //TODO: Change this input file path before start
+            var filePath = "D:\\Projects\\Interview\\SourceCode\\Console\\ConsoleApp\\Input.txt";
+
+            var lines = System.IO.File.ReadLines(filePath).ToList();
+
+            int rankedCount = Convert.ToInt32(lines[0].Trim());
+
+            List<int> ranked = lines[1].TrimEnd().Split(' ').ToList().Select(rankedTemp => Convert.ToInt32(rankedTemp)).ToList();
+
+            int playerCount = Convert.ToInt32(lines[2].Trim());
+
+            List<int> player = lines[3].TrimEnd().Split(' ').ToList().Select(playerTemp => Convert.ToInt32(playerTemp)).ToList();
+
+            List<int> result = GetRank(ranked, player);
+
+            Console.WriteLine(String.Join("\n", result));
+
+        }
+    }
+}
---
+++ Faq.Interview/Controllers/InterviewController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using FAQ.Interview.Helper;
+
+namespace Faq.Interview.Controllers
+{
+    public class InterviewController : Controller
+    {
+        public ActionResult Index(bool showInstructions = false)
+        {
+            if (showInstructions)
+                TempData["SHOW_INSTRUCTIONS"] = "TRUE";
+            else
+                TempData["SHOW_INSTRUCTIONS"] = "";
+
+            return View();
+        }
+
+        public ActionResult SaveData()
+        {
+            var model = new DinnerViewModel
+            {
+                Name = "This is a Name",
+                Description = "<p>This is a description</p>",
+                Sizes = new List<Size>
+                {
+                    new Size
+                    {
+                        Name = "Individual",
+                        Price = 1.5,
+                        Unit = .3
+                    },
+                    new Size
+                    {
+                        Name = "Small",
+                        Price = 2,
+                        Unit = 1
+                    },
+                    new Size
+                    {
+                        Name = "Large",
+                        Price = 3.5,
+                        Unit = 1.5
+                    }
+                }
+            };
+
+            return View(model);
+        }
+
+        [HttpPost, ValidateInput(false)]
+        [ValidateAntiForgeryToken]
+        public ActionResult SaveData(DinnerViewModel model)
+        {
+            return Json(model, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Sql()
+        {
+            return View();
+        }
+
+        public ActionResult Report(int? month = null)
+        {
+            //Use this year value
+            var year = DateTime.Today.Year;
+
+            var dataProvider = new DataProvider();
+            var data = dataProvider.Report(month);
+
+            return View();
+        }
+
+        public ActionResult Problem()
+        {
+            return View();
+        }
+    }
+}
